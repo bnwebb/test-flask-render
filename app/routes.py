95b2672b55d4f9f5994272f0d67030a94f614027
Brian Webb <brian.webbito@gmail.com>
2022-12-03 16:10:30 -0500

    Clean up form and add help text

--- app/routes.py
+++ app/routes.py
@@ -4,11 +4,9 @@ from app.forms import UserSubmitForm
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    form = UserSubmitForm(request.form)
-    if request.method == 'POST':
-        if form.validate_on_submit():
-            name = form.name.data
-            return redirect(url_for('success'))
+    form = UserSubmitForm()
+    if form.validate_on_submit():
+        return redirect(url_for('success'))
     return render_template('index.html', title='Home', form=form)
 
 @app.route('/success', methods=['GET'])
