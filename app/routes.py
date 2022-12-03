fd5bd4bf24790d5214fca70a6dc100db33f8ad46
Brian Webb <brian.webbito@gmail.com>
2022-12-03 15:37:01 -0500

    Add form and some bootstrap styling

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,16 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, request
 from app import app
+from app.forms import UserSubmitForm
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html', title='Home')
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    form = UserSubmitForm(request.form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            name = form.name.data
+            return redirect(url_for('success'))
+    return render_template('index.html', title='Home', form=form)
+
+@app.route('/success', methods=['GET'])
+def success():
+    return render_template('success.html')
